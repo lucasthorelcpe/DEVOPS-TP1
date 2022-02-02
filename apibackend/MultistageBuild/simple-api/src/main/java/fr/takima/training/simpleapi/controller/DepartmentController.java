2e9e3405064a07c3220b25acda0bf5a99802efa9
lucasthorelcpe <71006066+lucasthorelcpe@users.noreply.github.com>
2022-02-02 09:36:36 +0100

    Update DepartmentController.java

--- apibackend/MultistageBuild/simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
+++ apibackend/MultistageBuild/simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
@@ -5,10 +5,7 @@ import fr.takima.training.simpleapi.service.DepartmentService;
 import fr.takima.training.simpleapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
